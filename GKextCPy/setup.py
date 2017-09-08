2a12a71a29439df96717152b0dd2676839cebcb3
Elisabetta Ghisu <eghisu@bs-dalc01.ethz.ch>
2017-09-08 10:36:20 +0200

    small updates

--- GKextCPy/setup.py
+++ GKextCPy/setup.py
@@ -4,15 +4,15 @@
 setup.py file for SWIG 
 """
 
-from distutils.core import setup, Extension
-#from setuptools import setup, Extension
+#from distutils.core import setup, Extension
+from setuptools import setup, Extension
 
 
 GKextCPy_module = Extension('_GKextCPy', sources=['GKextCPy_wrap.cxx', 'GKextCPy.cpp'],swig_opts=['-c++'], extra_compile_args = ["-std=c++11"])
                            
 
 setup (name = 'GKextCPy',
-       version = '0.2.7',
+       version = '0.2.9',
        author      = "Elisabetta Ghisu",
        description = """Graph Kernels: building the extension Python module. This is a wrapper package from C++ to Python.""",
        ext_modules = [GKextCPy_module],
