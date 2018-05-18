f6eb9a3e1cf642e5276302fccf0a330d2114d883
Elisabetta Ghisu <eghisu@bs-dalc01.ethz.ch>
2018-05-18 17:23:29 +0200

    ubuntu fixed

--- GKextCPy/setup.py
+++ GKextCPy/setup.py
@@ -6,15 +6,16 @@ setup.py file for SWIG
 
 #from distutils.core import setup, Extension
 from setuptools import setup, Extension
-
+import numpy as np
 
 GKextCPy_module = Extension('_GKextCPy', sources=['GKextCPy_wrap.cxx', 'GKextCPy.cpp'],swig_opts=['-c++'], extra_compile_args = ["-std=c++11"])
                            
 
 setup (name = 'GKextCPy',
-       version = '0.3.0',
+       version = '0.3.1',
        author      = "Elisabetta Ghisu",
        description = """Graph Kernels: building the extension Python module. This is a wrapper package from C++ to Python.""",
+       include_dirs = [np.get_include()],
        ext_modules = [GKextCPy_module],
        py_modules = ["GKextCPy"],
        license = 'ETH Zurich',
