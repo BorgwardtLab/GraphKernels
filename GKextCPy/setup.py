34c954ee0deb0bb18b16366d9e76d997bf688038
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2018-06-27 16:38:22 +0200

    Updated setup file

--- GKextCPy/setup.py
+++ GKextCPy/setup.py
@@ -4,18 +4,23 @@
 setup.py file for SWIG 
 """
 
-#from distutils.core import setup, Extension
 from setuptools import setup, Extension
+import numpy as np
 
+GKextCPy_module = Extension('_GKextCPy',
+    sources = ['GKextCPy_wrap.cxx', 'GKextCPy.cpp'],
+    swig_opts = ['-c++'],
+    extra_compile_args = ['-std=c++11', '-O3'],
+    include_dirs = [np.get_include()]
+)
 
-GKextCPy_module = Extension('_GKextCPy', sources=['GKextCPy_wrap.cxx', 'GKextCPy.cpp'],swig_opts=['-c++'], extra_compile_args = ["-std=c++11"])
-                           
-
-setup (name = 'GKextCPy',
-       version = '0.3.0',
-       author      = "Elisabetta Ghisu",
-       description = """Graph Kernels: building the extension Python module. This is a wrapper package from C++ to Python.""",
-       ext_modules = [GKextCPy_module],
-       py_modules = ["GKextCPy"],
-       license = 'ETH Zurich',
-       )
+setup(name = 'GKextCPy',
+    version = '0.3.7',
+    author = "Elisabetta Ghisu",
+    description = """Graph Kernels: building the extension Python module. This is a wrapper package from C++ to Python.""",
+    include_dirs = [np.get_include()],
+    ext_modules = [GKextCPy_module],
+    py_modules = ["GKextCPy"],
+    setup_requires = ['numpy'],
+    license = 'ETH Zurich',
+)
