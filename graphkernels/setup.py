450a93d65f7d7a39682050451f6947d150cc4b89
Elisabetta Ghisu <elisabetta.ghisu@bsse.ethz.ch>
2018-06-28 14:01:16 +0200

    update version gk

--- graphkernels/setup.py
+++ graphkernels/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name = 'graphkernels',
-      version = '0.1.8',
+      version = '0.1.9',
 	description = 'Package for computing graph kernels',
 	url = 'https://github.com/eghisu/GraphKernels/tree/master/graphkernels',
 	author = 'Elisabetta Ghisu',
