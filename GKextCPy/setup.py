67ab114de864ef9626b88391e7f7a2e4705e39e4
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2018-06-28 13:53:50 +0200

    Searching for `eigen3` include directory

--- GKextCPy/setup.py
+++ GKextCPy/setup.py
@@ -5,22 +5,36 @@ setup.py file for SWIG
 """
 
 from setuptools import setup, Extension
-import numpy as np
+
+def get_include_dirs():
+    import pkgconfig
+    import numpy
+
+    if not pkgconfig.exists('eigen3'):
+        raise Exception, 'Missing `eigen3` library. Please install it using the package manager of your operating system'
+
+    np_include_dir = np.get_include()
+
+    # We have to throw away the `-I` part of the `pkgconfig` output
+    # because it is not part of the include directory.
+    eigen3_include_dir = pkgconfig.cflags('eigen3')[2:]
+
+    return [np_include_dir, eigen3_include_dir]
 
 GKextCPy_module = Extension('_GKextCPy',
     sources = ['GKextCPy_wrap.cxx', 'GKextCPy.cpp'],
     swig_opts = ['-c++'],
     extra_compile_args = ['-std=c++11', '-O3'],
-    include_dirs = [np.get_include()]
+    include_dirs = get_include_dirs()
 )
 
 setup(name = 'GKextCPy',
     version = '0.3.7',
     author = "Elisabetta Ghisu",
     description = """Graph Kernels: building the extension Python module. This is a wrapper package from C++ to Python.""",
-    include_dirs = [np.get_include()],
+    include_dirs = get_include_dirs(), # FIXME: do we have to specify this twice?
     ext_modules = [GKextCPy_module],
     py_modules = ["GKextCPy"],
-    setup_requires = ['numpy'],
+    setup_requires = ['pkgconfig', 'numpy'],
     license = 'ETH Zurich',
 )
